687568fbc3a9aa7c078c8dd795d066c7da55cf5a
Emiliano <emmiipaz@gmail.com>
2023-11-08 18:20:09 -0300

    cambios en el createClient

--- src/services/ClientService.ts
+++ src/services/ClientService.ts
@@ -4,27 +4,30 @@ import { Client } from "../entities/Client";
 import { AppDataSource } from "../index"
 import { RolesEnum } from "../entities/RolesEnum";
 import { error } from "console";
+import { type } from "os";
 
 interface IClientCreate {
     id?: number;
     firstName: string;
     lastName: string;
-    age: string;
+    age?: string;
     phoneNumber: string;
     email: string;
     address: string;
     birthDate: string;
     dni: string;
-    userName: string;
+    userName?: string;
     password: string;
 }
 
 class ClientService {
 
     clientRepository = AppDataSource.getRepository(Client)
+    
 
-    async create({ firstName, lastName, age, phoneNumber, email, address, birthDate, dni, userName, password }: IClientCreate) {
-        if (!firstName || !lastName || !age || !phoneNumber || !email || !address || !birthDate || !dni || !userName || !password) {
+    async create({ firstName, lastName, phoneNumber, email, address, birthDate, dni, password }: IClientCreate) {
+        console.log(typeof birthDate + " -- ")
+        if (!firstName || !lastName || !phoneNumber || !email || !address || !birthDate || !dni || !password) {
             throw new Error("Por favor complete todos los datos.");
         }
 
@@ -40,7 +43,13 @@ class ClientService {
             throw new Error("Email ya existe.");
         }
 
-        const client = new Client(firstName, lastName, age, phoneNumber, email, address, birthDate, dni, userName, password, RolesEnum.CLIENT)
+        const birthDateObj = new Date(birthDate);
+        const today = new Date();
+        const age = today.getFullYear() - birthDateObj.getFullYear();
+
+        const userName = generateRandomUsername(firstName, lastName);
+
+        const client = new Client(firstName, lastName, age.toString(), phoneNumber, email, address, birthDate, dni, userName, password, RolesEnum.CLIENT)
 
         await this.clientRepository.save(client);
 
@@ -128,6 +137,11 @@ class ClientService {
 
     }
 
+}
+function generateRandomUsername(firstName: string, lastName: string): string {
+    // Generar un nombre de usuario aleatorio (por ejemplo, concatenando las primeras letras del nombre y apellido)
+    const randomUsername = `${firstName[0]}${lastName[0]}_${Math.floor(Math.random() * 10000)}`;
+    return randomUsername;
 }
 
 export { ClientService };
